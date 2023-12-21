c91fac59b0e7baafa6ab4647cedbfaca694d42a5
polvonovsadriddin <144240322+polvonovsadriddin@users.noreply.github.com>
2023-12-22 01:20:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿class Person
+
+{
+    public string? Name { get; set; }
+    public virtual int Age
+    { get { return Age; }
+        set { if (value >= 0 && value <= 100) Age = value; }
+
+
+    }
+  
+
+    public virtual bool IsMarried { get; private set; }
+
+   
+}
